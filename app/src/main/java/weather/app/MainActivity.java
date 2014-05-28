b7096b66158d803abc5744ec62a4db66d8cca322
wongpet5 <peterskwongut@gmail.com>
2014-05-27 20:44:10 -0400

    Updated UI: Removed Drawers on the left side
    Removed Sliding Drawers on the left side.

--- app/src/main/java/weather/app/MainActivity.java
+++ app/src/main/java/weather/app/MainActivity.java
@@ -1,25 +1,41 @@
 package weather.app;
 
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Handler;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.view.*;
 import android.app.Fragment;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.app.FragmentManager;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import weather.app.HelperMethods.Weather_Location;
+import weather.app.HelperMethods.Weather_Network;
+import weather.app.HelperMethods.Weather_XMLParse;
 
 public class MainActivity extends ActionBarActivity {
 
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
 
-    private String mTitle = "";
+    private Handler handler;
+    private Context context = null;
+    private View v = null;
 
     @Override
     public View findViewById(int id) {
@@ -31,81 +47,148 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.drawer_list);
+        // Initialize the Handler
+        handler = new Handler();
+    }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(), R.layout.drawer_list_item, getResources().getStringArray(R.array.menu_array));
-        mDrawerList.setAdapter(adapter);
+    @Override
+    public void onStart()
+    {
+        // Call Parent
+        super.onStart();
 
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String[] menuItems = getResources().getStringArray(R.array.menu_array);
+        // Thread to initiate the call to get current location weather information
+        startThread();
+    }
 
-                mTitle = menuItems[position];
+    // Web Calls cannot be done on the main thread, therefore they will be done on a secondary thread
+    protected void startThread() {
+        Thread t = new Thread() {
+            public void run() {
 
-                if (position != 0)
-                {
-                    WebViewFragment rFragment = new WebViewFragment();
+                // Get current location
+                Weather_Location weatherLocation = new Weather_Location(getApplicationContext());
 
-                    Bundle data = new Bundle();
-                    data.putInt("position", position);
-                    data.putString("url", getUrl(position));
-                    rFragment.setArguments(data);
+                // Get Current Weather Conditions
+                Weather_Network weatherCall = new Weather_Network();
 
-                    FragmentManager fragmentManager = getFragmentManager();
-                    FragmentTransaction ft = fragmentManager.beginTransaction();
-                    ft.replace(R.id.content_frame, rFragment);
-                    ft.commit();
-                    mDrawerLayout.closeDrawer(mDrawerList);
+                String weatherXML = weatherCall.DownloadText(weatherLocation.GetLatitude(), weatherLocation.GetLongitude(), 1);
+                Log.d("Today Fragment", weatherXML);
+
+                // Parse Current Weather Conditions XML
+                final Weather_XMLParse weatherXMLParse = new Weather_XMLParse(weatherXML);
+                try {
+                    weatherXMLParse.ParseCurrentWeatherXML();
                 }
-                else if (position == 0)
-                {
-                    TodayFragment rFragment = new TodayFragment(getApplicationContext());
-                    Bundle data = new Bundle();
-                    data.putInt("position", position);
-                    data.putString("url", getUrl(position));
-                    rFragment.setArguments(data);
-
-                    FragmentManager fragmentManager = getFragmentManager();
-                    FragmentTransaction ft = fragmentManager.beginTransaction();
-                    ft.replace(R.id.content_frame, rFragment);
-                    ft.commit();
-                    mDrawerLayout.closeDrawer(mDrawerList);
+                catch (XmlPullParserException e1) {
+                    throw new RuntimeException(e1);
                 }
+                catch (IOException e2) {
+                    throw new RuntimeException(e2);
+                }
+
+                // Get Future Weather Conditions
+                String weatherJSONFuture = weatherCall.DownloadText(weatherLocation.GetLatitude(), weatherLocation.GetLongitude(), 2);
+
+                Log.d("Future Weather JSON", weatherJSONFuture);
+
+                //Set the values for the Widget Controls
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        SetUIControls(weatherXMLParse);
+                    }
+                });
             }
-        });
+        };
+        t.start();
     }
 
-    protected String getUrl(int position) {
-        switch (position) {
-            case 0:
-                return "http://javatechig.com";
-            case 1:
-                return "http://javatechig.com/category/android/";
-            default:
-                return "http://javatechig.com";
+    private void SetUIControls(Weather_XMLParse weatherXMLParse) {
+
+        // Set the Date
+        //TextView dateTextView = (TextView) getActivity().findViewById(R.id.DateTimeText);
+        //Calendar c = Calendar.getInstance();
+        //dateTextView.setText(c.getTime().toString());
+
+        // Set the Day of the Week
+        TextView dateTextView = (TextView) findViewById(R.id.Day);
+        Calendar c = Calendar.getInstance();
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+
+        String weekDay = "";
+        if (Calendar.MONDAY == dayOfWeek) {
+            weekDay = "Monday";
+        } else if (Calendar.TUESDAY == dayOfWeek) {
+            weekDay = "Tuesday";
+        } else if (Calendar.WEDNESDAY == dayOfWeek) {
+            weekDay = "Wednesday";
+        } else if (Calendar.THURSDAY == dayOfWeek) {
+            weekDay = "Thursday";
+        } else if (Calendar.FRIDAY == dayOfWeek) {
+            weekDay = "Friday";
+        } else if (Calendar.SATURDAY == dayOfWeek) {
+            weekDay = "Saturday";
+        } else if (Calendar.SUNDAY == dayOfWeek) {
+            weekDay = "Sunday";
         }
-    }
+        dateTextView.setText(weekDay);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Set the Month + Day
+        int month = c.get(Calendar.MONTH);
+        String dd = "";
+        if (month == Calendar.JANUARY) {
+            dd = "January " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.FEBRUARY) {
+            dd = "February " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.MARCH) {
+            dd = "March " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.APRIL) {
+            dd = "April " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.MAY) {
+            dd = "May " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.JUNE) {
+            dd = "June " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.JULY) {
+            dd = "July " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.AUGUST) {
+            dd = "August " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.SEPTEMBER) {
+            dd = "September " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.OCTOBER) {
+            dd = "October " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.NOVEMBER) {
+            dd = "November " + c.get(Calendar.DAY_OF_MONTH);
+        } else if (month == Calendar.DECEMBER) {
+            dd = "December " + c.get(Calendar.DAY_OF_MONTH);
         }
-        return super.onOptionsItemSelected(item);
-    }
+        TextView monthTextView = (TextView) findViewById(R.id.Month);
+        monthTextView.setText(dd);
 
+        // Set the City
+        TextView cityTextView = (TextView) findViewById(R.id.CityText);
+        cityTextView.setText(weatherXMLParse.getCurrentWeather().cityName);
+
+        // Set Current Temperature
+        TextView currentTemperatureText = (TextView) findViewById(R.id.CurrentTemperatureText);
+        currentTemperatureText.setText((int)Math.round(weatherXMLParse.getCurrentWeather().temperature) + " °C");
+
+        // Set Minimum Temperature
+        TextView minTemperatureText = (TextView) findViewById(R.id.MinimumTemperatureText);
+        minTemperatureText.setText("Low: " + (int)Math.round(weatherXMLParse.getCurrentWeather().minTemp) + " °C");
+
+        // Set Maximum Temperature
+        TextView maxTemperatureText = (TextView) findViewById(R.id.MaximumTemperatureText);
+        maxTemperatureText.setText("High: " + (int)Math.round(weatherXMLParse.getCurrentWeather().maxTemp) + " °C");
+
+        // Weather Image uses SVG
+        //ImageView weatherImage = (ImageView) getActivity().findViewById(R.id.WeatherImage);
+        //SVG svg = SVGParser.getSVGFromResource(getResources(), R.raw.cloud);
+        //Drawable drawable = svg.createPictureDrawable();
+        //weatherImage.setImageDrawable(drawable);
+
+        // Weather Image uses PNG
+        ImageView weatherImage = (ImageView) findViewById(R.id.WeatherImage);
+        weatherImage.setImageResource(R.raw.fair);
+    }
 }
